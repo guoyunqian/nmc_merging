dd78a712d9801b7e94046ae9b5710de210d0fa1b
anduin_zhao@sina.com <zh941638>
2021-09-27 09:52:05 +0800

    修改脚本文件，最后调用生成站点数据时，需要等待所有要素完成。

--- nmc.sh
+++ nmc.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 /bin/bash /space/cmadaas/dpl/NWFD01/code/nmc_merging/nmc_tmp.sh >/dev/null 2>&1 &
-pid=$!
+tmp_pid=$!
 
 sleep 2s
 /bin/bash /space/cmadaas/dpl/NWFD01/code/nmc_merging/nmc_pre.sh >/dev/null 2>&1 &
+pre_pid=$!
 sleep 2s
 /bin/bash /space/cmadaas/dpl/NWFD01/code/nmc_merging/nmc_rh.sh >/dev/null 2>&1 &
+rh_pid=$!
 sleep 2s
 /bin/bash /space/cmadaas/dpl/NWFD01/code/nmc_merging/nmc_pph.sh >/dev/null 2>&1 &
+pph_pid=$!
 sleep 2s
 /bin/bash /space/cmadaas/dpl/NWFD01/code/nmc_merging/nmc_uv.sh >/dev/null 2>&1 &
+uv_pid=$!
 
 #sleep 7m
-wait ${pid}
+wait ${tmp_pid}
 /bin/bash /space/cmadaas/dpl/NWFD01/code/nmc_merging/nmc_tmax.sh >/dev/null 2>&1 &
-pidtmx=$!
+tmx_pid=$!
 sleep 2s
 /bin/bash /space/cmadaas/dpl/NWFD01/code/nmc_merging/nmc_tmin.sh >/dev/null 2>&1 &
-pidtmi=$!
+tmi_pid=$!
 
-wait ${pidtmx}
-wait ${pidtmi}
+wait ${pre_pid}
+wait ${rh_pid}
+wait ${pph_pid}
+wait ${uv_pid}
+
+wait ${tmx_pid}
+wait ${tmi_pid}
 
 /bin/bash /space/cmadaas/dpl/NWFD01/code/nmc_grid2station/nmc_grid2station.sh >/dev/null 2>&1 &
